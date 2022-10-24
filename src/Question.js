6785b6859d423bb9630fee9de4493071a869db6f
devachra <devachr29@gmail.com>
2022-10-24 13:41:09 +0530

    according react.js

---
+++ src/Question.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ question }) => {
+  const { title, info } = question;
+  const [show, setShow] = useState(false);
+  const onShowHandler = () => {
+    setShow((prev) => !prev);
+  };
+
+  return (
+    <>
+      <article className="question">
+        <header>
+          <h4>{title} </h4>
+          <button className="btn" onClick={onShowHandler}>
+            {show ? <AiOutlineMinus /> : <AiOutlinePlus />}
+          </button>
+        </header>
+        {show && <p>{info} </p>}
+      </article>
+    </>
+  );
+};
+
+export default Question;
